4a81a478b20920e69f91df2f3e84317db4e895ff
HuyBuiQ <136289810+HuyBuiQ@users.noreply.github.com>
2024-03-14 08:12:53 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,16 @@
-public class MP3 {
+public class MP3 extends Item{
+    public int duration;
+
+    public MP3(java.lang.String name, java.lang.String description, java.lang.String ID, float price, int duration) {
+        super(name, description, ID, price);
+        this.duration = duration;
+    }
+    @Override
+    public void showInfo(){
+        System.out.println("Ten MP3: "+name);
+        System.out.println("Mo ta" + getDescription());
+        System.out.println("ID: "+ID);
+        System.out.println("Gia: "+price);
+        System.out.println("Do dai" +duration);
+    }
 }
