b24cfa542d1943f20c8683e88e2528d630cb7bd7
HuyBuiQ <136289810+HuyBuiQ@users.noreply.github.com>
2024-03-14 08:13:16 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
+
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("Sony Walkman","May nghe nhac xin xo dat tien","WM-001",399,100);
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("Lap trinh Java","Kien thuc lap trinh can ban","NEU-002",200,"Pham Xuan Lam",109,"Sach day lap trinh");
         book.showInfo();
+
+
 
     }
 }
