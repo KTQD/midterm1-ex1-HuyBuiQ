0ee8cfac120f1853c34e4cd09e5b1a0bee1d907d
HuyBuiQ <136289810+HuyBuiQ@users.noreply.github.com>
2024-03-14 08:12:13 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,22 @@
-public class Book {
+public class Book extends Item{
+    public String author;
+    public int numberOfPages;
+    public String genre;
+
+    public Book(java.lang.String name, java.lang.String description, java.lang.String ID, float price, java.lang.String author, int numberOfPages, java.lang.String genre) {
+        super(name, description, ID, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
+    @Override
+    public void showInfo(){
+        System.out.println("Ten sach: "+name);
+        System.out.println("Mo ta" + getDescription());
+        System.out.println("ID: "+ID);
+        System.out.println("Gia: "+price);
+        System.out.println("Tac gia" + author);
+        System.out.println("So trang: "+numberOfPages);
+        System.out.println("The loai: "+genre);
+    }
 }
